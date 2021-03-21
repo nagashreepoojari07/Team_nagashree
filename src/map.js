e9f45c1ed2cbd9555a0d6dc5855281bdc8deedf3
nagashreepoojari07 <56950098+nagashreepoojari07@users.noreply.github.com>
2021-03-21 16:13:00 +0530

    Add files via upload

---
+++ src/map.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MapImg from './COVID-19.jpg';
+import './map.css'
+function Map() {
+    return (
+        <div class="map">
+          <img src={MapImg}></img>
+        </div>
+    )
+}
+
+export default Map
