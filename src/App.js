2f5a673b8f76ae40e5e374019ad44922bfbab30d
nagashreepoojari07 <nagashreepoojari2017gmail.com>
2021-03-21 11:15:58 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './App.css';
+import FormControl from "@material-ui/core"
+function App() {
+  return (
+    <div className="App">
+      <h1>COVID-TRACKER</h1>
+
+    </div>
+  );
+}
+
+export default App;
